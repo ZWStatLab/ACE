583d5ddb3fa6ebcc9c3d7d9ff687548ce750c449
ZWStatLab <zeya.wang@uky.edu>
2025-10-15 11:56:15 -0400

    update

--- application/script/deep_clustering/DEPICT/run_num.py
+++ application/script/deep_clustering/DEPICT/run_num.py
@@ -4,25 +4,25 @@ import time
 datasets = ['USPS', 'MNIST-test', 'FRGC', 'YTF', 'CMU-PIE'] 
 
 for dataset in datasets:
-  if dataset == 'YTF':
-    numcls = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-  elif dataset == 'USPS':
-    numcls = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-  elif dataset == 'MNIST-test':
-    numcls = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-  elif dataset == 'FRGC':
-    numcls = [10, 15, 20, 25, 30, 35, 40, 45, 50]
-  elif dataset == 'CMU-PIE':
-    numcls = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    if dataset == 'YTF':
+        numcls = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    elif dataset == 'USPS':
+        numcls = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    elif dataset == 'MNIST-test':
+        numcls = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    elif dataset == 'FRGC':
+        numcls = [10, 15, 20, 25, 30, 35, 40, 45, 50]
+    elif dataset == 'CMU-PIE':
+        numcls = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-  for numcl in numcls:
-    try:
-        cmd = 'CUDA_VISIBLE_DEVICES=0 python DEPICT_num.py --dataset={} --num_clusters={}'.format(dataset, numcl)
-        print(cmd)
-        os.system(cmd)
-        while not os.path.isfile('done.o'):
-            time.sleep(0.1)
-        os.system('rm done.o')
-    except:
-        print('continue')
-        continue
+    for numcl in numcls:
+        try:
+            cmd = 'CUDA_VISIBLE_DEVICES=0 python DEPICT_num.py --dataset={} --num_clusters={}'.format(dataset, numcl)
+            print(cmd)
+            os.system(cmd)
+            while not os.path.isfile('done.o'):
+                    time.sleep(0.1)
+            os.system('rm done.o')
+        except:
+            print('continue')
+            continue
